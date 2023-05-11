61084644f8158edc0e5f51b08872c7cc8db41d8e
Sinol <103750421+Sinol95@users.noreply.github.com>
2023-05-11 09:46:09 +0200

    Add files via upload

--- src/main/java/Escape.java
+++ src/main/java/Escape.java
@@ -1,36 +1,35 @@
 package main.java;
 
 import java.util.ArrayList;
-
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import main.java.entity.Arrow;
 import main.java.entity.Entity;
 import main.java.entity.Monster;
-import main.java.entity.NonPlayableCharacter;
 import main.java.entity.Player;
-import main.java.entity.projectile.Bullet;
+import main.java.entity.NonPlayableCharacter;
 
 public class Escape extends Application {
 	
 	private Canvas canvas;
 	private Camera camera;
 	private Renderer renderer;
-	private Environment environment;
+	private Map map;
 	private Input input;
 	private Player player;
-	private Monster monster;
+	private ArrayList<Entity> entities;
+	private ArrayList<Arrow> arrows;
+	private ProgressBar life;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -41,35 +40,25 @@ public class Escape extends Application {
 		
 		VBox root = new VBox();
 		root.getStyleClass().add("root");
-		Scene scene = new Scene(root,1000,750);
+		Scene scene = new Scene(root,1600,900);
 		
 		canvas = new Canvas(scene.getWidth(), scene.getHeight()-100);
-		canvas.setFocusTraversable(true);
-		environment = new Environment();
+		map = new Map();
 		renderer = new Renderer(canvas);
-		camera = new Camera(9, 51);
+		camera = new Camera(100, 200);
 		root.getChildren().add(canvas);
-
-		player = new Player(9, 51);
-		monster = new Monster(9,51);
-		environment.addEntity(player);
-		environment.addEntity(monster);
-		environment.addEntity(new NonPlayableCharacter(9,51));
 		
-		ProgressBar life = new ProgressBar(1);
-		life.setPrefSize(scene.getWidth()/2, scene.getHeight()/4);
+		player = new Player(9, 51);
+		entities = new ArrayList<Entity>();
+		entities.add(player);
+		entities.add(new Monster(43,51));
+		entities.add(new NonPlayableCharacter(25, 51));
+		arrows = new ArrayList<Arrow>();
+		
+		life = new ProgressBar(1);
+		life.setPrefSize(scene.getWidth(), scene.getHeight()/4);
 		life.setStyle("-fx-accent: green;");
-		HBox inventory = new HBox();
-		for (int i = 0; i < 3; i++) {
-			Button inv = new Button();
-			inv.setPrefSize(50, 50);
-			inventory.getChildren().add(inv);
-		}
-		inventory.setAlignment(Pos.CENTER);
-		inventory.setPrefWidth(scene.getWidth()/2);
-		inventory.setSpacing(75);
-		HBox bottom = new HBox(life, inventory);
-		bottom.getStyleClass().add("bottom");
+		HBox bottom = new HBox(life);
 		root.getChildren().add(bottom);
 		
 		input = new Input(scene);
@@ -90,11 +79,37 @@ public class Escape extends Application {
 	}
 
 	private void tick(ActionEvent actionEvent) {
+		
 		player.handleInput(input);
-		environment.tickEntities();
+		Monster monster = null;
+		
+		for (Entity entity: entities) {
+			if(entity instanceof Player) {
+				((Player)entity).tick(map);
+			}
+			else if (entity instanceof Monster){
+				monster = ((Monster)entity);
+				monster.tick(map);
+				
+				if(monster.getShoot() == 1) arrows.add(new Arrow(monster.getX(),monster.getY(),-0.1,monster));
+				else if(monster.getShoot() == 2) arrows.add(new Arrow(monster.getX(),monster.getY(),0.1,monster));
+			}
+		}
+		
+		for (Arrow arrow : arrows) {
+			arrow.tick(map, player);
+			if(arrow.getHit()==2) player.setLife(player.getLife()-0.1);
+		}
+		arrows.removeIf(arrow -> arrow.getHit()!=0); //Flèche supprimée en cas de collision
+		
 		camera.setTarget_x(player.getX()+player.getWidth()/2);
 		camera.setTarget_y(player.getY());
 		camera.tick();
-		renderer.render(environment,camera);
+		renderer.render(map,camera,entities,arrows);
+		
+		life.setProgress(player.getLife());
+		if(life.getProgress() <= 0) System.exit(0);
+		if(life.getProgress()<0.3) life.setStyle("-fx-accent: red;");
+		else if(life.getProgress()<0.6) life.setStyle("-fx-accent: orange;");
 	}
 }
